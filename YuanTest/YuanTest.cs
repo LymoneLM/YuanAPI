f85458e3aa5a4075faef4b82cc5ebebdcb98d88d
LymoneLM <chengziorange233@qq.com>
2025-10-22 20:38:03 +0800

    Test: 新增手动测试项目YuanTest

---
+++ YuanTest/YuanTest.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using BepInEx;
+using YuanAPI;
+
+namespace YuanTest
+{
+    [BepInDependency(YuanAPIPlugin.MODGUID)]
+    [BepInPlugin(MODGUID, MODNAME, VERSION)]
+    public class YuanTest : BaseUnityPlugin
+    {
+        public const string MODNAME = "YuanTest";
+        public const string MODGUID = YuanAPIPlugin.MODGUID + "." + MODNAME;
+        public const string VERSION = "1.0.0";
+
+        public void Awake()
+        {
+            using var propReg = new PropRegistry(MODNAME);
+            propReg.PropList.Add(new PropData()
+            {
+                ID = "TestThing",
+                Category = (int)PropCategory.Weapon,
+                PropEffect = new Dictionary<int, int>()
+                {
+                    {(int)PropEffectType.Might,100}
+                },
+                Text = ["测试物品","TestThing"],
+                PrefabPath = "AllProp/1"
+            });
+        }
+    }
+}
