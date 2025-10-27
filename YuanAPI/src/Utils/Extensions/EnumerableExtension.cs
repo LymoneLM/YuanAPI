23bfaff5d6327f602e4e6e7798d3d5bc4b6b1f45
LymoneLM <chengziorange233@qq.com>
2025-10-27 18:56:03 +0800

    feat(Utils/Extensions): 新增带index的可枚举ForEach扩展

---
+++ YuanAPI/src/Utils/Extensions/EnumerableExtension.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace YuanAPI;
+
+public static class EnumerableExtension
+{
+    public static void ForEach<T>(this IEnumerable<T> enumerable, Action<T, int> action)
+    {
+        var index = 0;
+        foreach (var item in enumerable)
+        {
+            action(item, index);
+            index++;
+        }
+    }
+}
