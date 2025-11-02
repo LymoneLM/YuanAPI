889d99795bc9a67b10f6c61bedf1eb0a9a50a4a5
LymoneLM <chengziorange233@qq.com>
2025-11-03 05:21:15 +0800

    fix(Localization): 修复设置面板注入错误

--- YuanAPI/src/Localization/Patches/SetPanelPatch.cs
+++ YuanAPI/src/Localization/Patches/SetPanelPatch.cs
@@ -19,7 +19,7 @@ public class SetPanelPatch
     [HarmonyPatch(typeof(SetPanel), "InitShow")]
     public static void LoadLanguageOption(SetPanel __instance)
     {
-        var langDropdown = __instance.transform.Find("LoadSpeed").Find("AllClass").GetComponent<Dropdown>();
+        var langDropdown = __instance.transform.Find("Language").Find("AllClass").GetComponent<Dropdown>();
         langDropdown.options =
             Localization.GetAllShowNames().Select(name => new Dropdown.OptionData(name)).ToList();
         langDropdown.value = Mainload.SetData[4];
