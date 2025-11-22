5030c37ae0965e6b3ade79f56f11deeaa5b919b1
LymoneLM <chengziorange233@qq.com>
2025-11-23 01:38:34 +0800

    feat(Tools): 新增货币工具

---
+++ YuanAPI/src/Tools/CoinTool.cs
@@ -0,0 +1,14 @@
+﻿namespace YuanAPI;
+
+public class CoinTool
+{
+    public bool TryChangeCoins(int count)
+    {
+        if (count < 0 && FormulaData.GetCoinsNum() < -count)
+        {
+            return false;
+        }
+        FormulaData.ChangeCoins(count);
+        return true;
+    }
+}
